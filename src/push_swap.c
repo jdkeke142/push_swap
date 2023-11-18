ef66b76ddad990358d1981b927bfac1bf143f4aa
jdkeke142 <kjimenez@student.42angouleme.fr>
2023-11-18 16:34:13 +0100

    Removed garbage comments & useless includes

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,42 +6,13 @@
 /*   By: kjimenez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 15:16:42 by kjimenez          #+#    #+#             */
-/*   Updated: 2023/11/18 15:57:12 by kjimenez         ###   ########.fr       */
+/*   Updated: 2023/11/18 16:33:38 by kjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_list.h"
-#include "ft_stdio.h"
-#include "actions.h"
-#include "markup.h"
-#include "indexing.h"
+#include "ft_stdlib.h"
 #include "move.h"
 #include "stack.h"
-#include <stdio.h>
-#include "ft_stdlib.h"
-
-//Parts: Solve function that returns a list of actions without modifying the passed list
-//- Indexing
-//- From stack A to stack B
-//- From stack B to stack A
-//- Align stack A
-//- Simply stack A
-
-/*static void	print_content2(void *content)
-{
-	t_move	*move;
-
-	move = (t_move *) content;
-	printf("Move stack : %d action : %d\n", move->stack, move->action);
-}*/
-
-/*static void	print_content3(void *content)
-{
-	t_markup	markup;
-
-	markup = *((t_markup *) content);
-	printf("Number is : %d Keep : %d\n", markup.number, markup.keep);
-}*/
 
 static t_list	*solve(int *numbers, int numbers_count,
 	t_markup_mode markup_mode)
