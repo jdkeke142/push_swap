c7aa9c8cad394dabc1519663e531676413c1cf95
jdkeke142 <kjimenez@student.42angouleme.fr>
2023-11-18 16:48:02 +0100

    Broke stack_a_to_b into multiple functions

--- src/stack_a_to_b.c
+++ src/stack_a_to_b.c
@@ -6,7 +6,7 @@
 /*   By: kjimenez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 19:02:17 by kjimenez          #+#    #+#             */
-/*   Updated: 2023/11/18 16:36:21 by kjimenez         ###   ########.fr       */
+/*   Updated: 2023/11/18 16:47:19 by kjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,32 @@ static int	sa_needed(t_stack_a *stack_a, t_markup_mode mode)
 	return (keep_swap > keep_without_swap);
 }
 
+static void	stack_a_to_b_rotate(t_stack_a *stack_a, t_list *moves)
+{
+	rotate_lst(stack_a->markup);
+	rotate_lst(stack_a->indexed);
+	ft_lstadd_back(moves, move_new(STACK_A, ACT_ROTATE));
+}
+
+static void	stack_a_to_b_push(t_stack_a *stack_a, t_list *stack_b_markup,
+	t_list *moves)
+{
+	push_lst(stack_a->markup, stack_b_markup);
+	free(ft_lstdel_front(stack_a->indexed));
+	ft_lstadd_back(moves, move_new(STACK_A, ACT_PUSH));
+}
+
+static void	stack_a_to_b_swap(t_stack_a *stack_a, t_markup_mode markup_mode,
+	t_list *moves)
+{
+	swap_lst(stack_a->markup);
+	swap_lst(stack_a->indexed);
+	ft_lstadd_back(moves, move_new(STACK_A, ACT_SWAP));
+	ft_lstdestroy(stack_a->markup, &free);
+	stack_a->markup = get_markup(stack_a->indexed,
+			stack_a->markup_head, markup_mode);
+}
+
 void	stack_a_to_b(t_stack_a *stack_a, t_list *stack_b_markup,
 	t_list *moves, t_markup_mode markup_mode)
 {
@@ -39,25 +65,10 @@ void	stack_a_to_b(t_stack_a *stack_a, t_list *stack_b_markup,
 	{
 		stack_a_first_elem = (t_markup *) ft_lstfirst(stack_a->markup);
 		if (sa_needed(stack_a, markup_mode))
-		{
-			swap_lst(stack_a->markup);
-			swap_lst(stack_a->indexed);
-			ft_lstadd_back(moves, move_new(STACK_A, ACT_SWAP));
-			ft_lstdestroy(stack_a->markup, &free);
-			stack_a->markup = get_markup(stack_a->indexed,
-					stack_a->markup_head, markup_mode);
-		}
+			stack_a_to_b_swap(stack_a, markup_mode, moves);
 		else if (stack_a_first_elem->keep == 0)
-		{
-			push_lst(stack_a->markup, stack_b_markup);
-			free(ft_lstdel_front(stack_a->indexed));
-			ft_lstadd_back(moves, move_new(STACK_A, ACT_PUSH));
-		}
+			stack_a_to_b_push(stack_a, stack_b_markup, moves);
 		else
-		{
-			rotate_lst(stack_a->markup);
-			rotate_lst(stack_a->indexed);
-			ft_lstadd_back(moves, move_new(STACK_A, ACT_ROTATE));
-		}
+			stack_a_to_b_rotate(stack_a, moves);
 	}
 }
